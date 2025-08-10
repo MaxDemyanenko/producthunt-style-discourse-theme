d1914a48fdce3ca84bd3619fbf6cc993a468801f
Maksim Demyanenko <mtretre22@gmail.com>
2025-08-10 11:46:25 +0300

    fixes

--- javascripts/discourse/initializers/ph-cards.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import template from "discourse/templates/list/topic-list-item";
-
-export default apiInitializer("1.40.0", (api) => {
-  api.replaceRawTemplate("list/topic-list-item", template);
-});
